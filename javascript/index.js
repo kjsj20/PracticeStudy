58125a1a4df2c810a6c627b43846d16c7ea3d42f
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-09-09 08:57:16 +0900

    no message

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,23 @@
-// String
-// const what = "jaesung";
-// console.log(what);
-// Boolean
-// const what = false;
-// Number
-// const what = 666;
-//float
-// const what = 55.1;
+// String const what = "jaesung"; console.log(what); Boolean const what = false;
+// Number const what = 666; float const what = 55.1; const daysOFWeek = ["js",
+// "26",true,"Seoul"] array
+const jsInfo = {
+    name: "js",
+    age: 26,
+    gender: "Male",
+    isHandsome: true,
+    favMovies: [
+        "Along the gods", "LOTR", "Oldboy"
+    ],
+    favFood: [
+        {
+            name: "Kimchi",
+            fatty: false
+        }, 
+        {
+            name: "Cheese burger",
+            fatty: true
+        }
+    ]
+} //Object
+console.log(jsInfo.favFood[0].name);
