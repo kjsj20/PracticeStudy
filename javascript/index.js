4423c0efc4d5fd241f2211476415c418dfcb8c93
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-09-10 03:52:48 +0900

    no message

--- javascript/index.js
+++ javascript/index.js
@@ -13,19 +13,47 @@ const jsInfo = {
         {
             name: "Kimchi",
             fatty: false
-        }, 
-        {
+        }, {
             name: "Cheese burger",
             fatty: true
         }
     ]
 } //Object
 
-// console.log(jsInfo, console);
+// console.log(jsInfo, console); function sayHello(name, age){     return `Hello
+// ${name} you are ${age} years old` } const greetJs = sayHello("Js", 14) const
+// calculator = {     plus: function(a, b){         return a + b;     },
+// minus: function(a, b){         return a - b;     },     multiply: function(a,
+// b){         return a * b;     },     divide: function(a, b){         return a
+// / b;     },     pow: function(a, b){         return a ** b;     } };
+// console.log(greetJs) const plus = calculator.plus(5, 5); const minus =
+// calculator.minus(3, 1); const multiply = calculator.multiply(2, 2); const
+// divide = calculator.divide(6, 2); const pow = calculator.pow(2, 4);
+// console.log(`plus : ${plus}, minus : ${minus}, multiply : ${multiply}, divide
+// : ${divide}, pow : ${pow}`); const title = document.getElementById("title")
+// 아래것과 같은의미 querySelector 노드의 첫번째 자식을 반화해오는 내장함수
+const title = document.querySelector("#title");
+console.dir(document);
+// document.getElementById("title").style.color = "red"; console.error("Fuck");
+title.innerHTML = "hi js";
+// title.style.color = "red";
+document.title = " I own you now";
 
-function sayHello(potato, chicken){
-    console.log('Hello!', potato, chicken);
+function handleClcik() {
+    // console.log("I have been resized");
+    title.style.color = 'yellow';
 }
 
-sayHello("js", 15);
-console.log()
+title.addEventListener("click", handleClcik);
+
+const age = prompt("How old are you");
+
+if(age > 18 && age <= 21) {
+    console.log("you can drink but you should not");
+} else if (age > 21) {
+    console.log("go ahed");
+} else {
+    console.log("to young");
+}
+
+// console.log(age);
