e33bbcba6f75477c9f2f9a013b664590f6472978
kjsj20 <43256316+kjsj20@users.noreply.github.com>
2020-09-13 22:19:11 +0900

    캐릭터 위아래 움직이는거 작동

--- SimpleProject/CharAct/index.js
+++ SimpleProject/CharAct/index.js
@@ -1,4 +1,5 @@
 var lf = 0;
+var ud = 0;
 
 function moveChar(){
     if(event.keyCode === 37){
@@ -6,6 +7,14 @@ function moveChar(){
     }
     else if(event.keyCode === 39){
         moveRight();
+    }
+    else if(event.keyCode === 38){
+        ud-= 10;
+        pic.style.top = ud + "px";
+    }
+    else if(event.keyCode === 40){
+        ud+= 10;
+        pic.style.top = ud + "px";
     }
 }
 
@@ -19,3 +28,6 @@ function moveRight(){
     document.getElementById("pic").style.left = lf + "px";
 }
 
+function moveUpDown(){
+    pic = document.querySelector("#pic");
+}
