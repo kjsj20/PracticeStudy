18415aeb85518fa9f8843c9cd6f8e8d91088f388
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-10-07 14:35:17 +0900

    개인공부 커밋

--- SimpleProject/rockmanPositon/Rockimgload.js
+++ SimpleProject/rockmanPositon/Rockimgload.js
@@ -1,11 +1,10 @@
 // 록맨 캐릭터를 그린다.
 class Rockimgload{
-    constructor(x, velX){
+    constructor(x, velX, src){
         this.x = x;
         this.velX = velX;
         this.img = document.createElement("img");
-
-        this.img.src = "../images/rockman/leftmain.png"
+        this.img.src = src;
         this.img.style.left = `${x}px`
         this.img.style.width = `${80}px`;
         this.img.style.height = `${90}px`;
@@ -20,5 +19,15 @@ class Rockimgload{
 
     render(){
         this.img.style.left = `${this.x}px`;
+    }
+
+    moveimg(){
+        if(loadRock.img.src = "../images/rockman/left1.png") {
+            loadRock.img.src = "../images/rockman/left2.png";
+        } else if (loadRock.img.src = "../images/rockman/left2.png"){
+            loadRock.img.src = "../images/rockman/left3.png"
+        } else {
+            loadRock.img.src = "../images/rockman/left1.png"
+        }
     }
 }
--- SimpleProject/rockmanPositon/rockmanPosition.js
+++ SimpleProject/rockmanPositon/rockmanPosition.js
@@ -12,7 +12,7 @@ function init(){
 }
 
 function createRock(){
-     loadRock = new Rockimgload(200,0);
+     loadRock = new Rockimgload(200,0, "../images/rockman/rightMain.png");
 }
 
 function gameLoop(){
@@ -24,20 +24,33 @@ window.addEventListener("load", function(){
     document.body.addEventListener("keydown", function(e){
         var key = e.keyCode;
         switch(key){
-            case 37: loadRock.velX = -5; break;
-            case 39: loadRock.velX = 5; break;
+            case 37: 
+            loadRock.velX = -5;
+            loadRock.img.src = "../images/rockman/left1.png"
+            break;
+            case 39: 
+            loadRock.velX = 5; 
+            loadRock.img.src = "../images/rockman/right1.png";
+            break;
         }
     });
 
     document.body.addEventListener("keyup", function(e){
         var key = e.keyCode;
         switch(key){
-            case 37: loadRock.velX = 0; break;
-            case 39: loadRock.velX = 0; break;
+            case 37: 
+            loadRock.velX = 0;  
+            loadRock.img.src = "../images/rockman/leftMain.png";
+            break;
+            case 39: 
+            loadRock.velX = 0; 
+            loadRock.img.src = "../images/rockman/rightMain.png";
+            break;
         }
     });
     
     init();
     createRock();
     setInterval("gameLoop()", 10);
+    setInterval("loadRock.moveimg()", 100);
 });
