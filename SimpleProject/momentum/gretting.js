0a6ad46e7b9a5daa9345a64085200cab4a4ba372
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-10-12 10:21:06 +0900

    c

--- SimpleProject/momentum/gretting.js
+++ SimpleProject/momentum/gretting.js
@@ -5,6 +5,21 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser", 
         SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
@@ -15,7 +30,8 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-            // she is not
+        // she is not
+        askForName();
     }  else {
         // she is
         paintGreeting(currentUser);
